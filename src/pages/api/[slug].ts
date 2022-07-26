72ef1f7071238b21d16d2c8f6cffe25725fa4107
amar-jay <abdelmanan.abdelrahman03@gmail.com>
2022-07-26 12:36:58 +0300

    Link shortener cconfiged

---
+++ src/pages/api/[slug].ts
@@ -0,0 +1,32 @@
+import { NextApiRequest, NextApiResponse } from "next";
+import { prisma } from "../../db/prisma";
+
+const getUrl = async (req: NextApiRequest, res: NextApiResponse) => {
+  const slug = req.query["slug"];
+
+  if (!slug || typeof slug !== "string") {
+    res.statusCode = 404;
+    res.send(JSON.stringify({ message: "slug is undefined" }));
+
+    return;
+  }
+
+  const data = await prisma.shortLink.findFirst({
+    where: {
+      slug: {
+        equals: slug!,
+      },
+    },
+  })!;
+
+  if (!data || data?.url === null) {
+    res.statusCode = 404;
+    res.send(JSON.stringify({ message: "Link not found" }));
+
+    return;
+  }
+
+  res.redirect(data.url);
+};
+
+export default getUrl;

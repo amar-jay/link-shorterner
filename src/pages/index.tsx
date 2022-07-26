f18fcf4fc7a79f12291329026b4bdc81ec7efe02
amar-jay <abdelmanan.abdelrahman03@gmail.com>
2022-07-27 00:01:30 +0300

    SEO

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -8,19 +8,15 @@ const Home: NextPage = () => {
       <Head>
         <title>Link Shorterner</title>
         <meta name="description" content="A link shorterner generator app" />
-        <link rel="icon" href="/favicon.ico" />
         <meta name="viewport" content="initial-scale=1.0, width=device-width" />
         <meta name="description" content="A link Shorterner" />
         <meta name="author" content="Amar Jay" />
-        <link rel="apple-touch-icon" href="apple-touch-icon.png" />
-        <link rel="shortcut icon" href="/bulb.svg" type="image/x-icon" />
         <meta name="twitter:title" content="A link Shorterner" />
         <meta name="twitter:card" content="summary_large_image" />
         <meta name="twitter:site" content="@amarjay" />
         <meta property="og:site_name" content="A link Shorterner App" />
         <meta name="og:title" content="A link Shorternerr" />
         <meta property="og:type" content="website" />
-        <meta property="og:image" content="/dye-0.jpg" />
       </Head>
 
       <main className="items-center justify-evenly flex h-[90vh] flex-col">

bc5266b74f54b25718c8b04a2989eb4451d2b5cc
amar-jay <abdelmanan.abdelrahman03@gmail.com>
2022-07-26 13:51:05 +0300

    Middleware resolved

---
+++ src/middleware.ts
@@ -0,0 +1,19 @@
+import { NextFetchEvent, NextRequest, NextResponse } from "next/server";
+
+export async function middleware(req: NextRequest, ev: NextFetchEvent) {
+  const slug = req.nextUrl.pathname.split("/").pop();
+
+  const slugFetch = await fetch(`${req.nextUrl.origin}/api/fetchUrl/${slug}`);
+  if (slugFetch.status === 404) {
+    return NextResponse.redirect(req.nextUrl.origin);
+  }
+  const data = await slugFetch.json();
+
+  if (data?.url) {
+    return NextResponse.redirect(data.url);
+  }
+}
+
+export const config = {
+  matcher: "/:slug",
+};

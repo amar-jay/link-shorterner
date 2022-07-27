5ccfb180fa9d26002ca6be31b7bfce50e1dc6622
amar-jay <abdelmanan.abdelrahman03@gmail.com>
2022-07-27 10:00:51 +0300

    Still striying

--- src/middleware.ts
+++ src/middleware.ts
@@ -2,19 +2,19 @@ import { NextFetchEvent, NextRequest, NextResponse } from "next/server";
 
 export async function middleware(req: NextRequest, _: NextFetchEvent) {
   const slug = req.nextUrl.pathname.split("/").pop();
-  if (slug?.includes('.')) {
-    return
+  if (slug?.includes(".")) {
+    return;
   }
   const slugFetch = await fetch(`${req.nextUrl.origin}/api/fetchUrl/${slug}`);
   if (slugFetch.status === 404) {
-    return NextResponse.redirect(`${req.nextUrl.origin}/`);
+    return NextResponse.redirect(`${req.nextUrl.origin}/error/404`);
   }
   const { data } = await slugFetch.json();
 
   if (data?.url) {
     return NextResponse.redirect(data.url);
   }
-//  return NextResponse.redirect(`${req.nextUrl.origin}/error/404`);
+  return NextResponse.redirect(`${req.nextUrl.origin}/error/404`);
 }
 
 export const config = {
